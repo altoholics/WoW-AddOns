d3a46b0f12b8f1eff8d67a479cd41b9c7efca553
nazuraki <nazuraki@gmail.com>
2024-08-16 22:48:17 -0400

    [warbandeer] show frame when opening

--- Warbandeer/core.lua
+++ Warbandeer/core.lua
@@ -10,7 +10,37 @@ function f:OnEvent(event, ...)
 end
 f:SetScript("OnEvent", f.OnEvent)
 
-function ns.Open() ns.Print("open main window") end
+-- https://wowpedia.fandom.com/wiki/Category:HOWTOs
+-- addon compartment, settings scroll templates: https://warcraft.wiki.gg/wiki/Patch_10.1.0/API_changes
+-- settings changes: https://warcraft.wiki.gg/wiki/Patch_11.0.2/API_changes
+
+function ns.Open()
+    -- https://www.reddit.com/r/wowaddondev/comments/1cc2qgj/creating_a_wow_addon_part_2_creating_a_frame/
+    -- https://www.wowinterface.com/forums/showthread.php?t=40444
+    local frame = CreateFrame("Frame", "Warbandeer", UIParent, "BasicFrameTemplateWithInset")
+    frame:SetMovable(true)
+    frame:EnableMouse(true)
+    frame:RegisterForDrag("LeftButton")
+    frame:SetScript("OnDragStart", function(self)
+        self:StartMoving()
+    end)
+    frame:SetScript("OnDragStop", function(self)
+        self:StopMovingOrSizing()
+    end)
+
+    frame:SetPoint("CENTER")
+    frame:SetSize(200, 200)
+    local tex = frame:CreateTexture("ARTWORK")
+    tex:SetAllPoints()
+    -- tex:SetColorTexture(1.0, 0.5, 0, 0.5)
+
+    frame.TitleBg:SetHeight(30)
+    frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+    frame.title:SetPoint("TOPLEFT", frame.TitleBg, "TOPLEFT", 5, -3)
+    frame.title:SetText("Warbandeer")
+
+    ShowUIPanel(frame)
+end
 
 local defaults = {
 }
@@ -31,4 +61,15 @@ f:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 function Warbandeer_OnAddonCompartmentClick(addonName, buttonName)
     ns.Print("OnAddonCompartmentClick", addonName, buttonName)
+    ns.Open()
 end
+
+-- https://wowpedia.fandom.com/wiki/Create_a_WoW_AddOn_in_15_Minutes#Slash_commands
+SLASH_WARBAND1 = "/warband"
+SLASH_WARBAND2 = "/wb"
+
+SlashCmdList.WARBAND = function(msg, editBox)
+    ns.Open()
+end
+
+-- https://wowpedia.fandom.com/wiki/Create_a_WoW_AddOn_in_15_Minutes#Options_Panel
