6260e14941bdf3863a21ff5528212ff2b484d41a
nazuraki <nazuraki@gmail.com>
2024-08-30 21:14:35 -0400

    hide buttons out of combat with no target

--- ShadowsOfUI/ActionBars.lua
+++ ShadowsOfUI/ActionBars.lua
@@ -4,7 +4,7 @@ local ui = ns.g.ui
 local Frame = ui.Frame
 
 local HideUIPanel, ShowUIPanel, UnitExists, UnitAffectingCombat = ns.g.HideUIPanel, ns.g.ShowUIPanel, ns.g.UnitExists, ns.g.UnitAffectingCombat
-local MainMenuBar = ns.g.MainMenuBar
+local MainMenuBar, Bar2 = ns.g.MainMenuBar, ns.g.MultiBarBottomLeft
 
 local BarControl = Frame:new{
   events = {"PLAYER_ENTERING_WORLD", "PLAYER_REGEN_DISABLED", "PLAYER_REGEN_ENABLED", "PLAYER_TARGET_CHANGED"},
@@ -13,24 +13,37 @@ BarControl:hide()
 
 function BarControl:update()
   if self.hasTarget or self.inCombat then
-    ShowUIPanel(MainMenuBar)
+    for _,btn in pairs(MainMenuBar.actionButtons) do
+      btn:Show()
+    end
+    for _,btn in pairs(Bar2.actionButtons) do
+      btn:Show()
+    end
   else
-    HideUIPanel(MainMenuBar)
+    for _,btn in pairs(MainMenuBar.actionButtons) do
+      btn:Hide()
+    end
+    for _,btn in pairs(Bar2.actionButtons) do
+      btn:Hide()
+    end
   end
 end
 
 -- enter combat
 function BarControl:PLAYER_REGEN_DISABLED()
   self.inCombat = true
+  self:update()
 end
 
 -- leave combat
 function BarControl:PLAYER_REGEN_ENABLED()
   self.inCombat = false
+  self:update()
 end
 
 function BarControl:PLAYER_TARGET_CHANGED()
   self.hasTarget = UnitExists("target")
+  self:update()
 end
 
 function BarControl:PLAYER_ENTERING_WORLD(login, reload)
@@ -40,11 +53,5 @@ function BarControl:PLAYER_ENTERING_WORLD(login, reload)
     self.hasTarget = UnitExists("target")
     self.inCombat = UnitAffectingCombat("player")
     self:update()
-
-    -- for k,v in pairs(MainMenuBar) do
-    --   if type(v) ~= "function" then
-    --     print(k)
-    --   end
-    -- end
   end
 end
