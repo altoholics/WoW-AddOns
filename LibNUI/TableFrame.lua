b017a3d4731f4537bb72c2b21e4212a40340f235
nazuraki <nazuraki@gmail.com>
2024-09-09 15:16:02 -0400

    refactoring

--- LibNUI/TableFrame.lua
+++ LibNUI/TableFrame.lua
@@ -11,6 +11,28 @@ local Class, Frame, BgFrame = ns.util.Class, ui.Frame, ui.BgFrame
 --   CELL_WIDTH   int          - width of cells in pixels
 --   CELL_HEIGHT  int          - height of cells in pixels
 
+local TableRow = Class(BgFrame, function(o)
+  o:topLeft(0, #o.rows * -o.position.height - o.position.insetTop)
+  o:withLabel({
+    text = o.label,
+    position = {ns.ui.edge.Left, 2, 0},
+  })
+end, {
+  level = 2,
+  backdrop = {alpha = 0},
+})
+
+local TableCol = Class(BgFrame, function(o)
+  o.topLeft(#o.cols * o.CELL_WIDTH + o.insetLeft, 0)
+  o:withLabel({
+    text = o.label,
+    position = {ns.ui.edge.TopLeft, 3, -6},
+  })
+end, {
+  level = 1,
+  backdrop = {alpha = 0},
+})
+
 -- making a table: https://www.wowinterface.com/forums/showthread.php?t=58670
 local TableFrame = Class(Frame, function(o)
   o.numCols = o.columns or #o.colNames
@@ -19,48 +41,20 @@ local TableFrame = Class(Frame, function(o)
   local offsetX = o.rowNames ~= nil and o.CELL_WIDTH or 0
   local offsetY = o.colNames ~= nil and o.CELL_HEIGHT or 0
 
-  local frame = o.frame
   o.cols = {}
   o.rows = {}
-  local cols, rows = o.cols, o.rows
 
   if o.colNames then
     local colHeight = o.CELL_HEIGHT * (o.numRows + 1)
     for i=1,#o.colNames do
-      cols[i] = BgFrame:new{
-        parent = frame,
-        level = 1,
-        backdrop = {alpha = 0},
-        position = {
-          width = o.CELL_WIDTH,
-          height = colHeight,
-          topLeft = {(i-1) * o.CELL_WIDTH + offsetX, 0},
-        },
-      }
-      cols[i]:withLabel({
-        text = o.colNames[i],
-        position = {"TOPLEFT", 3, -6},
-      })
+      o:addCol(o.colNames[i], o.CELL_WIDTH, colHeight, offsetY)
     end
   end
 
   if o.rowNames then
     local rowWidth = o.CELL_WIDTH * (o.numCols + 1)
     for i=1,#o.rowNames do
-      rows[i] = BgFrame:new{
-        parent = frame,
-        level = 2,
-        backdrop = {alpha = 0},
-        position = {
-          width = rowWidth,
-          height = o.CELL_HEIGHT,
-          topLeft = {0, (i-1) * -o.CELL_HEIGHT - offsetY},
-        },
-      }
-      rows[i]:withLabel({
-        text = o.rowNames[i],
-        position = {"LEFT", 2, 0},
-      })
+      o:addRow(o.rowNames[i], rowWidth, o.CELL_HEIGHT, offsetX)
     end
   end
 
@@ -70,6 +64,28 @@ local TableFrame = Class(Frame, function(o)
   end
 end)
 ui.TableFrame = TableFrame
+
+function TableFrame:addCol(text, width, height, insetLeft)
+  table.insert(self.cols, TableCol:new{
+    label = text,
+    position = {
+      width = width,
+      height = height,
+      insetLeft = insetLeft,
+    },
+  })
+end
+
+function TableFrame:addRow(text, width, height, insetTop)
+  table.insert(self.rows, TableRow:new{
+    label = text,
+    position = {
+      width = width,
+      height = height,
+      insetTop = insetTop,
+    },
+  })
+end
 
 function TableFrame:row(n) return self.rows[n] end
 function TableFrame:col(n) return self.cols[n] end
--- Warbandeer/window.lua
+++ Warbandeer/window.lua
@@ -30,7 +30,7 @@ local function CreateMainFrame()
 
     -- color the backgrounds of the rows by class color
     for i=1,ns.g.NUM_CLASSES do
-        t:row(i):backdropColor(CLASSES[i].color.r, CLASSES[i].color.g, CLASSES[i].color.b, 0.2)
+        t:row(i).frame:backdropColor(CLASSES[i].color.r, CLASSES[i].color.g, CLASSES[i].color.b, 0.2)
     end
 
     for name,data in pairs(ns.api.GetAllCharacters()) do
