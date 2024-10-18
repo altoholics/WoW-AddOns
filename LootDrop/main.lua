4cb6a28c2b65eefb9dc28c78bc8027b5ecf66d42
nazuraki <nazuraki@gmail.com>
2024-10-18 09:23:41 -0400

    clean up to new standards, show recent history on click

--- LootDrop/main.lua
+++ LootDrop/main.lua
@@ -1,47 +1,141 @@
 local ADDON_NAME, ns = ...
 
-local ui = LibNUI
-
 local GetMoney, GetCoinTextureString, GetItemInfo = GetMoney, C_CurrencyInfo.GetCoinTextureString, C_Item.GetItemInfo
+local ExtractHyperlinkString = ExtractHyperlinkString
 
-local function max(a, b)
-  return a > b and a or b
+LibNAddOn{
+  name = ADDON_NAME,
+  addOn = ns,
+  db = {
+    name = "LootDropDB",
+    defaults = {
+      trackedItems = {n=0},
+    },
+  },
+  slashCommands = {
+    warband = {"/lootdrop", "/ld"},
+  },
+}
+
+local Class, tinsert, tremove = ns.lua.Class, ns.lua.tinsert, ns.lua.tremove
+local max = ns.lua.max
+local ui = LibNUI
+local Frame, CleanFrame, Label, Texture = ui.Frame, ui.CleanFrame, ui.Label, ui.Texture
+local UIParent = ns.wowui.UIParent
+
+local HistoryItem = Class(Frame, function(self)
+  local name, _, qual, _, _, _, _, _, _, tex = GetItemInfo(self.link)
+
+  self.icon = Texture:new{
+    parent = self,
+    layer = "ARTWORK",
+    coords = {0.07, 0.93, 0.07, 0.93},
+    path = tex,
+    position = {
+      topLeft = {},
+      bottomLeft = {},
+      width = 30,
+    },
+  }
+  self.name = Label:new{
+    parent = self,
+    text = name,
+    position = {
+      topLeft = {self.frame, ui.edge.TopLeft, 30, -4},
+      right = {self.frame, ui.edge.Right},
+      height = 26,
+    },
+    justifyH = "LEFT",
+  }
+end, {
+  position = {
+    height = 30,
+  },
+  background = {0, 0, 0, 0.5},
+})
+
+function HistoryItem:Update(link)
+  self.link = link
+  local name, _, qual, _, _, _, _, _, _, tex = GetItemInfo(self.link)
+  self.icon:setTexture(tex)
+  self.name:Text(name)
 end
 
-local Bucket = ui.Frame:new{
+local History = Class(CleanFrame, function(self)
+  self.history = {}
+end,{
+  parent = UIParent,
+  position = {
+    width = 150,
+    height = 12,
+    hide = true,
+  },
+  background = {0, 0, 0, 0.2},
+  events = {"CHAT_MSG_LOOT"},
+})
+
+function History:AddItem(link)
+  if #self.history < 10 then
+    tinsert(self.history, HistoryItem:new{
+      parent = self,
+      link = link,
+      position = {
+        bottom = {self.frame, ui.edge.Bottom, 0, 32 * #self.history + 2},
+        left = {self.frame, ui.edge.Left, 2, 0},
+        right = {self.frame, ui.edge.Right, -2, 0},
+      },
+    })
+  else
+    -- move oldest to end of list
+    local hi = tremove(self.history, 1)
+    hi:Update(link)
+    tinsert(self.history, hi)
+    -- reposition
+    for i=1,#self.history do
+      self.history[i]:bottom(self.frame, ui.edge.Bottom, 0, 32 * (i-1) + 2)
+    end
+  end
+  self:height(32 * #self.history + 2)
+end
+
+function History:CHAT_MSG_LOOT(text, playerName, _, _, playerName2)
+  if playerName == playerName2 or playerName2 == "" then -- our loot
+    local _, _, link = ExtractHyperlinkString(text)
+    if link then self:AddItem(link) end
+  end
+end
+
+local Bucket = Class(Frame, function(self)
+  self:makeDraggable()
+  self:makeContainerDraggable()
+  self:withTextureBackground("topBorder", {
+    color = {0, 0, 0},
+    position = {
+      topLeft = {0, 1},
+      topRight = {},
+    },
+  })
+  self.money = Label:new{
+    parent = self,
+    position = { topRight = {-2, -2} },
+  }
+
+  self.fadeDelay = 10000
+  self.fadeDuration = 5000
+  self.tracking = {n=0}
+end, {
   parent = UIParent,
   position = {
     width = 100,
     height = 20,
     bottomRight = {-300, 30},
   },
-  events = {"PLAYER_ENTERING_WORLD", "PLAYER_MONEY", "ADDON_LOADED"},
-  onLoad = function(self)
-    self:makeDraggable()
-    self:makeContainerDraggable()
-    self:withTextureBackground("topBorder", {
-      color = {0, 0, 0},
-      clamp = {
-        {ui.edge.TopLeft, 0, 1},
-        {ui.edge.TopRight},
-      },
-    })
-    self.money = self.frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    self.money:SetPoint("TOPRIGHT", -2, -2)
-    self.money:SetText("")
-
-    -- https://us.forums.blizzard.com/en/wow/t/help-me-with-lua-aniamtion/379238
-    self.anim1 = self.frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    self.anim1:SetPoint("TOPRIGHT", 20, -2)
-
-    self.fadeDelay = 10000
-    self.fadeDuration = 5000
-    self.tracking = {n=0}
-  end
-}
+  events = {"PLAYER_ENTERING_WORLD", "PLAYER_MONEY"},
+  scripts = {"OnEnter", "OnLeave", "OnMouseUp"},
+})
 
 function Bucket:checkSize()
-  local w = max(100, self.money:GetUnboundedStringWidth())
+  local w = max(100, self.money.label:GetUnboundedStringWidth())
   self.frame:SetWidth(w + 4)
 end
 
@@ -58,9 +152,9 @@ function Bucket:onUpdate(elapsed)
     if self.fadeTimer <= 0 then
       self:stopUpdates()
       self.fadeTimer = -1
-      self.frame:SetAlpha(0)
+      self.frame:SetAlpha(0.15)
     else
-      self.frame:SetAlpha(self.fadeTimer / self.fadeDuration)
+      self.frame:SetAlpha(0.15 + 0.85 * self.fadeTimer / self.fadeDuration)
     end
   end
 end
@@ -68,7 +162,7 @@ end
 function Bucket:updateMoney()
   local money = GetMoney()
   local s = GetCoinTextureString(money)
-  self.money:SetText(s)
+  self.money:Text(s)
   -- reset timers
   self.fadeTimer = -1
   self.fadeWait = self.fadeDelay
@@ -81,59 +175,72 @@ function Bucket:PLAYER_MONEY()
   self:updateMoney()
 end
 
+local ExtractHyperlinkString, GetItemInfoFromHyperlink = ExtractHyperlinkString, GetItemInfoFromHyperlink -- luacheck: globals ExtractHyperlinkString GetItemInfoFromHyperlink
 -- https://wowpedia.fandom.com/wiki/CHAT_MSG_LOOT
-function Bucket:CHAT_MSG_LOOT(text, playerName, langName, chanName, playerName2, specialFlags, zoneChannelID, channelIndex, channelBaseName, langID, lineID, guid, bnSenderID, isMobile, isSubtitle, hideSenderInLetterbox, suppressRaidIcons)
+function Bucket:CHAT_MSG_LOOT(text, playerName, _, _, playerName2) -- langName, chanName, specialFlags, zoneChannelID, channelIndex, channelBaseName, langID, lineID, guid, bnSenderID, isMobile, isSubtitle, hideSenderInLetterbox, suppressRaidIcons
   if playerName == playerName2 or playerName2 == "" then -- our loot
-    -- extract item link: https://wowpedia.fandom.com/wiki/ItemLink
-    -- |cffxxxxxx|Hitem:payload|h[text]|h|r
-    local x = string.find(text, "|cff")
-    local e = string.find(text, "|r", x)
-    if x and e then
-      local itemLink = string.sub(text, x, e+1)
-      if self.db.trackedItems[itemLink] then
+    local link = ExtractHyperlinkString(text)
+    if link then
+      local itemId = GetItemInfoFromHyperlink(link)
+      if self.db.trackedItems[itemId] then
         -- todo
       end
+      print(link)
     end
-  end
-end
-
-local defaults = {trackedItems = {n=0}}
-function Bucket:ADDON_LOADED(name)
-  if ADDON_NAME == name then
-    self.db = LootDropDB or CopyTable(defaults)
   end
 end
 
 function Bucket:startTracking(itemName)
   local name, link, _, _, _, _, _, _, _, textureID = GetItemInfo(itemName)
-  if name and not self.db.trackedItems[link] then
+  local itemId = GetItemInfoFromHyperlink(link)
+  if name and not self.db.trackedItems[itemId] then
     -- if we weren't watching loot, start
     if self.db.trackedItems.n == 0 then
       self.frame:RegisterEvent("CHAT_MSG_LOOT")
     end
     self.db.trackedItems.n = self.db.trackedItems.n + 1
-    self.db.trackedItems[link] = {name=name, textureID = textureID}
+    self.db.trackedItems[itemId] = {id=itemId, name=name, textureID = textureID}
     print("Now tracking ", link)
   end
 end
 
 function Bucket:stopTracking(itemName)
-  local name, link, _, _, _, _, _, _, _, textureID = GetItemInfo(itemName)
+  local name, link = GetItemInfo(itemName)
   if name and self.db.trackedItems[link] then
+    local itemId = GetItemInfoFromHyperlink(link)
     self.db.trackedItems.n = self.db.trackedItems.n - 1
-    self.db.trackedItems[link] = nil
+    self.db.trackedItems[itemId] = nil
     print("No longer tracking ", link)
   end
 end
 
-function Bucket:PLAYER_ENTERING_WORLD(login, reload)
+function Bucket:OnLogin(login, reload)
   self:updateMoney()
 end
 
-SLASH_LOOTDROP1 = "/lootdrop"
-SLASH_LOOTDROP2 = "/ld"
+function Bucket:OnEnter()
+  print("bucket enter")
+end
 
-function SlashCmdList.LOOTDROP(msg)
+function Bucket:OnLeave()
+  print("bucket leave")
+end
+
+function Bucket:OnMouseUp()
+  ns.history:toggle()
+end
+
+
+function ns:onLoad()
+  ns.bucket = Bucket:new{}
+  ns.history = History:new{
+    position = {
+      bottomRight = {ns.bucket.frame, ui.edge.TopRight, 0, 5},
+    },
+  }
+end
+
+function ns:SlashCmd(_, msg) -- cmd, msg
   local _, _, cmd, args = string.find(msg, "(%w+) ?(.*)")
   if "add" == cmd then
     Bucket:startTracking(args)
